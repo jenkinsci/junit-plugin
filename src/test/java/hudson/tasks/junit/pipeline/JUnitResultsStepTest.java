1b4a5dda0ba41cd6fe1dc4d0617db020b857ae8b
Andrew Bayer <andrew.bayer@gmail.com>
2017-07-27 10:22:31 -0400

    Tests for single invocation and multiple invocations

--- src/test/java/hudson/tasks/junit/pipeline/JUnitResultsStepTest.java
+++ src/test/java/hudson/tasks/junit/pipeline/JUnitResultsStepTest.java
@@ -1,8 +1,14 @@
 package hudson.tasks.junit.pipeline;
 
+import hudson.FilePath;
 import hudson.model.Result;
+import hudson.tasks.junit.TestResult;
 import hudson.tasks.junit.TestResultAction;
+import hudson.tasks.junit.TestResultTest;
+import org.jenkinsci.plugins.workflow.actions.TagsAction;
 import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition;
+import org.jenkinsci.plugins.workflow.flow.FlowExecution;
+import org.jenkinsci.plugins.workflow.graph.FlowNode;
 import org.jenkinsci.plugins.workflow.job.WorkflowJob;
 import org.jenkinsci.plugins.workflow.job.WorkflowRun;
 import org.junit.ClassRule;
@@ -11,6 +17,10 @@ import org.junit.Test;
 import org.jvnet.hudson.test.BuildWatcher;
 import org.jvnet.hudson.test.JenkinsRule;
 
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 
 public class JUnitResultsStepTest {
@@ -48,5 +58,91 @@ public class JUnitResultsStepTest {
         WorkflowRun r = rule.buildAndAssertSuccess(j);
         assertNull(r.getAction(TestResultAction.class));
         rule.assertLogContains("None of the test reports contained any result", r);
+    }
+
+    @Test
+    public void singleStep() throws Exception {
+        WorkflowJob j = rule.jenkins.createProject(WorkflowJob.class, "singleStep");
+        j.setDefinition(new CpsFlowDefinition("stage('first') {\n" +
+                "  node {\n" +
+                "    junitResults(testResults: '*.xml')\n" +
+                "  }\n" +
+                "}\n", true));
+        FilePath ws = rule.jenkins.getWorkspaceFor(j);
+        FilePath testFile = ws.child("test-result.xml");
+        testFile.copyFrom(TestResultTest.class.getResource("junit-report-1463.xml"));
+
+        WorkflowRun r = rule.buildAndAssertSuccess(j);
+        TestResultAction action = r.getAction(TestResultAction.class);
+        assertNotNull(action);
+        assertEquals(1, action.getResult().getSuites().size());
+        assertEquals(6, action.getTotalCount());
+
+        FlowExecution execution = r.getExecution();
+
+        FlowNode junitNode = execution.getNode("7");
+        assertNotNull(junitNode);
+        System.err.println(junitNode.getActions());
+        TagsAction tagsAction = junitNode.getAction(TagsAction.class);
+        assertNotNull(tagsAction);
+        assertEquals("true", tagsAction.getTagValue(JUnitResultsStep.HAS_TEST_RESULTS_TAG_NAME));
+
+        TestResult nodeTests = action.getResult().getResultByRunAndNode(r.getExternalizableId(), junitNode.getId());
+        assertNotNull(nodeTests);
+        assertEquals(1, nodeTests.getSuites().size());
+        assertEquals(6, nodeTests.getTotalCount());
+    }
+
+    @Test
+    public void twoSteps() throws Exception {
+        WorkflowJob j = rule.jenkins.createProject(WorkflowJob.class, "singleStep");
+        j.setDefinition(new CpsFlowDefinition("stage('first') {\n" +
+                "  node {\n" +
+                "    junitResults(testResults: 'first-result.xml')\n" +
+                "    junitResults(testResults: 'second-result.xml')\n" +
+                "  }\n" +
+                "}\n", true));
+        FilePath ws = rule.jenkins.getWorkspaceFor(j);
+        FilePath testFile = ws.child("first-result.xml");
+        testFile.copyFrom(TestResultTest.class.getResource("junit-report-1463.xml"));
+        FilePath secondTestFile = ws.child("second-result.xml");
+        secondTestFile.copyFrom(TestResultTest.class.getResource("junit-report-2874.xml"));
+
+        WorkflowRun r = rule.buildAndAssertSuccess(j);
+        TestResultAction action = r.getAction(TestResultAction.class);
+        assertNotNull(action);
+        assertEquals(2, action.getResult().getSuites().size());
+        assertEquals(7, action.getTotalCount());
+
+        FlowExecution execution = r.getExecution();
+
+        FlowNode firstNode = execution.getNode("7");
+        FlowNode secondNode = execution.getNode("8");
+        assertNotNull(firstNode);
+
+        TagsAction firstTags = firstNode.getAction(TagsAction.class);
+        assertNotNull(firstTags);
+        assertEquals("true", firstTags.getTagValue(JUnitResultsStep.HAS_TEST_RESULTS_TAG_NAME));
+
+        TestResult firstTests = action.getResult().getResultByRunAndNode(r.getExternalizableId(), firstNode.getId());
+        assertNotNull(firstTests);
+        assertEquals(1, firstTests.getSuites().size());
+        assertEquals(6, firstTests.getTotalCount());
+
+        assertNotNull(secondNode);
+        TagsAction secondTags = secondNode.getAction(TagsAction.class);
+        assertNotNull(secondTags);
+        assertEquals("true", secondTags.getTagValue(JUnitResultsStep.HAS_TEST_RESULTS_TAG_NAME));
+
+        TestResult secondTests = action.getResult().getResultByRunAndNode(r.getExternalizableId(), secondNode.getId());
+        assertNotNull(secondTests);
+        assertEquals(1, secondTests.getSuites().size());
+        assertEquals(1, secondTests.getTotalCount());
+
+        TestResult combinedTests = action.getResult().getResultByRunAndNodes(r.getExternalizableId(),
+                Arrays.asList(firstNode.getId(), secondNode.getId()));
+        assertNotNull(combinedTests);
+        assertEquals(2, combinedTests.getSuites().size());
+        assertEquals(7, combinedTests.getTotalCount());
     }
 }
