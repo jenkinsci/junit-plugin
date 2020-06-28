08e4df75a43ffc1213dc5d31b4eb6a7f3a741db4
Tim Jacomb <timjacomb1+github@gmail.com>
2020-06-28 16:51:15 +0100

    Stack graph

--- src/main/java/hudson/tasks/test/TestResultTrendChart.java
+++ src/main/java/hudson/tasks/test/TestResultTrendChart.java
@@ -13,24 +13,24 @@ public class TestResultTrendChart {
         TestResultTrendSeriesBuilder builder = new TestResultTrendSeriesBuilder();
         LinesDataSet dataSet = builder.createDataSet(configuration, results);
 
-        LinesChartModel model = new LinesChartModel(); // TODO: should the setters be mandatory in constructor?
+        LinesChartModel model = new LinesChartModel();
         model.setDomainAxisLabels(dataSet.getDomainAxisLabels());
         model.setBuildNumbers(dataSet.getBuildNumbers());
 
-        LineSeries passed = new LineSeries("Passed", Palette.BLUE.getNormal(),
-                LineSeries.StackedMode.SEPARATE_LINES, LineSeries.FilledMode.FILLED);
-        passed.addAll(dataSet.getSeries(TestResultTrendSeriesBuilder.PASSED_KEY));
-        model.addSeries(passed);
-
         LineSeries failed = new LineSeries("Failed", Palette.RED.getNormal(),
-                LineSeries.StackedMode.SEPARATE_LINES, LineSeries.FilledMode.FILLED);
+                LineSeries.StackedMode.STACKED, LineSeries.FilledMode.FILLED);
         failed.addAll(dataSet.getSeries(TestResultTrendSeriesBuilder.FAILED_KEY));
         model.addSeries(failed);
 
         LineSeries skipped = new LineSeries("Skipped", Palette.GRAY.getNormal(),
-                LineSeries.StackedMode.SEPARATE_LINES, LineSeries.FilledMode.FILLED);
+                LineSeries.StackedMode.STACKED, LineSeries.FilledMode.FILLED);
         skipped.addAll(dataSet.getSeries(TestResultTrendSeriesBuilder.SKIPPED_KEY));
         model.addSeries(skipped);
+
+        LineSeries passed = new LineSeries("Passed", Palette.BLUE.getNormal(),
+                LineSeries.StackedMode.STACKED, LineSeries.FilledMode.FILLED);
+        passed.addAll(dataSet.getSeries(TestResultTrendSeriesBuilder.PASSED_KEY));
+        model.addSeries(passed);
 
         return model;
     }
