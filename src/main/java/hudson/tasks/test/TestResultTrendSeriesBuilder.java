aedbe0e4964cf332c43027b2d47dc21718aa5192
Tim Jacomb <timjacomb1+github@gmail.com>
2020-08-10 08:09:36 +0100

    JENKINS-63339 Fix aggregated report trend (#140)

--- src/main/java/hudson/tasks/test/TestResultTrendSeriesBuilder.java
+++ src/main/java/hudson/tasks/test/TestResultTrendSeriesBuilder.java
@@ -14,10 +14,13 @@ public class TestResultTrendSeriesBuilder extends SeriesBuilder<AbstractTestResu
     @Override
     protected Map<String, Integer> computeSeries(AbstractTestResultAction testResultAction) {
         Map<String, Integer> series = new HashMap<>();
-        series.put(TOTALS_KEY, testResultAction.getTotalCount());
-        series.put(PASSED_KEY, testResultAction.getPassedTests().size());
-        series.put(FAILED_KEY, testResultAction.getFailCount());
-        series.put(SKIPPED_KEY, testResultAction.getSkipCount());
+        int totalCount = testResultAction.getTotalCount();
+        int failCount = testResultAction.getFailCount();
+        int skipCount = testResultAction.getSkipCount();
+        series.put(TOTALS_KEY, totalCount);
+        series.put(PASSED_KEY, totalCount - failCount - skipCount);
+        series.put(FAILED_KEY, failCount);
+        series.put(SKIPPED_KEY, skipCount);
         return series;
     }
 }
