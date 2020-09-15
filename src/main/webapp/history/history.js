0178d80cfbad0086b6b469e80b1de2cdc948d982
Tim Jacomb <timjacomb1+github@gmail.com>
2020-09-15 21:13:28 +0100

    Fix carousel

--- src/main/webapp/history/history.js
+++ src/main/webapp/history/history.js
@@ -1,6 +1,7 @@
 /* global jQuery3, view, echartsJenkinsApi */
 (function ($) {
     redrawTrendCharts();
+    storeAndRestoreCarousel('trend-carousel');
 
     /**
      * Activate tooltips.
@@ -22,7 +23,7 @@
         view.getTestDurationTrend(function (lineModel) {
             echartsJenkinsApi.renderZoomableTrendChart('test-duration-trend-chart', lineModel.responseJSON, redrawTrendCharts);
         });
-        
+
         /**
          * Creates a build trend chart that shows the number of issues for a couple of builds.
          * Requires that a DOM <div> element exists with the ID '#severities-trend-chart'.
@@ -30,5 +31,26 @@
         view.getTestResultTrend(function (lineModel) {
             echartsJenkinsApi.renderZoomableTrendChart('test-result-trend-chart', lineModel.responseJSON, redrawTrendCharts);
         });
+    }
+
+    /**
+     * Store and restore the selected carousel image in browser's local storage.
+     * Additionally, the trend chart is redrawn.
+     *
+     * @param {String} carouselId - ID of the carousel
+     */
+    function storeAndRestoreCarousel (carouselId) {
+        const carousel = $('#' + carouselId);
+        carousel.on('slid.bs.carousel', function (e) {
+            localStorage.setItem(carouselId, e.to);
+            const chart = $(e.relatedTarget).find('>:first-child')[0].echart;
+            if (chart) {
+                chart.resize();
+            }
+        });
+        const activeCarousel = localStorage.getItem(carouselId);
+        if (activeCarousel) {
+            carousel.carousel(parseInt(activeCarousel));
+        }
     }
 })(jQuery3);
