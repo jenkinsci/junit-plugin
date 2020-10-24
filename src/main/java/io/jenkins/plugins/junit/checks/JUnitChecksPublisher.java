cc06ba717b2073c2f6315580755c64217cca0149
babusekaran <sekaranbabu@ymail.com>
2020-10-24 22:26:02 +0200

    switching to length() for size calculation

--- src/main/java/io/jenkins/plugins/junit/checks/JUnitChecksPublisher.java
+++ src/main/java/io/jenkins/plugins/junit/checks/JUnitChecksPublisher.java
@@ -11,7 +11,6 @@ import io.jenkins.plugins.checks.api.ChecksOutput;
 import io.jenkins.plugins.checks.api.ChecksPublisher;
 import io.jenkins.plugins.checks.api.ChecksPublisherFactory;
 import io.jenkins.plugins.checks.api.ChecksStatus;
-import java.nio.charset.StandardCharsets;
 import java.util.List;
 import org.apache.commons.lang.StringUtils;
 import org.jenkinsci.plugins.displayurlapi.DisplayURLProvider;
@@ -62,7 +61,7 @@ public class JUnitChecksPublisher {
 
             for (CaseResult failedTest: failedTests) {
                 String testReport = mapFailedTestToTestReport(failedTest);
-                int messageSize = testReport.getBytes(StandardCharsets.UTF_8).length + builder.toString().getBytes(StandardCharsets.UTF_8).length;
+                int messageSize = testReport.length() + builder.toString().length();
                 // to ensure text size is withing check API message limit
                 if (messageSize > (MAX_MSG_SIZE_TO_CHECKS_API - 1024)){
                     builder.append("\n")
