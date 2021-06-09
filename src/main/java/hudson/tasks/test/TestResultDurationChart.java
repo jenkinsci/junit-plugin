993fbcf8a9691983cb9862d38fbf921e8ed69f90
Ulli Hafner <ullrich.hafner@gmail.com>
2021-06-09 14:10:55 +0200

    Simplify API of `LineChart`.

--- src/main/java/hudson/tasks/test/TestResultDurationChart.java
+++ src/main/java/hudson/tasks/test/TestResultDurationChart.java
@@ -1,25 +1,23 @@
 package hudson.tasks.test;
 
+import java.util.List;
+
 import edu.hm.hafner.echarts.ChartModelConfiguration;
 import edu.hm.hafner.echarts.LineSeries;
 import edu.hm.hafner.echarts.LinesChartModel;
 import edu.hm.hafner.echarts.LinesDataSet;
 import edu.hm.hafner.echarts.Palette;
-import hudson.tasks.junit.TestDurationResultSummary;
-import hudson.tasks.junit.TrendTestResultSummary;
-import java.util.List;
 
-import static hudson.tasks.test.TestDurationTrendSeriesBuilder.SECONDS;
-import static hudson.tasks.test.TestResultTrendSeriesBuilder.FAILED_KEY;
-import static hudson.tasks.test.TestResultTrendSeriesBuilder.PASSED_KEY;
-import static hudson.tasks.test.TestResultTrendSeriesBuilder.SKIPPED_KEY;
+import hudson.tasks.junit.TestDurationResultSummary;
+
+import static hudson.tasks.test.TestDurationTrendSeriesBuilder.*;
 
 public class TestResultDurationChart {
-    
-    public LinesChartModel create(List<TestDurationResultSummary> results) {
+
+    public LinesChartModel create(final List<TestDurationResultSummary> results) {
         LinesDataSet dataset = new LinesDataSet();
         results.forEach(result -> dataset.add(result.getDisplayName(), result.toMap(), result.getBuildNumber()));
-        
+
         return getLinesChartModel(dataset);
     }
 
@@ -31,16 +29,13 @@ public class TestResultDurationChart {
         return getLinesChartModel(dataSet);
     }
 
-    private LinesChartModel getLinesChartModel(LinesDataSet dataSet) {
-        LinesChartModel model = new LinesChartModel();
-        model.setDomainAxisLabels(dataSet.getDomainAxisLabels());
-        model.setBuildNumbers(dataSet.getBuildNumbers());
-
+    private LinesChartModel getLinesChartModel(final LinesDataSet dataSet) {
+        LinesChartModel model = new LinesChartModel(dataSet);
         LineSeries duration = new LineSeries(SECONDS, Palette.GREEN.getNormal(),
                 LineSeries.StackedMode.STACKED, LineSeries.FilledMode.FILLED);
         duration.addAll(dataSet.getSeries(SECONDS));
         model.addSeries(duration);
-        
+
         return model;
     }
 }
