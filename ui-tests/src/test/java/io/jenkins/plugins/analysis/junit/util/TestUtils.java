b3160f02cc4e212ea3cf45c5d32b6cca36675802
Michael Mueller <michael.mueller@hm.edu>
2022-01-20 23:19:53 +0100

    fixed test.

--- ui-tests/src/test/java/io/jenkins/plugins/analysis/junit/util/TestUtils.java
+++ ui-tests/src/test/java/io/jenkins/plugins/analysis/junit/util/TestUtils.java
@@ -20,6 +20,7 @@ public class TestUtils {
 
     /**
      * Creates a freestyle Job with resources and runs build with expected build result.
+     *
      * @param abstractJUnitTestBaseClass the caller's test class
      * @param resourcePaths resource paths of test result reports
      * @param expectedBuildResult expected build results for assertion
@@ -27,27 +28,7 @@ public class TestUtils {
      */
     public static Build createFreeStyleJobAndRunBuild(AbstractJUnitTest abstractJUnitTestBaseClass,
             List<String> resourcePaths, String expectedBuildResult) {
-        return createFreeStyleJobAndRunBuild(abstractJUnitTestBaseClass, resourcePaths, expectedBuildResult,
-                false, false, false);
-    }
-
-    /**
-     * Creates a freestyle Job with resources and runs build with expected build result.
-     * @param abstractJUnitTestBaseClass the caller's test class
-     * @param resourcePaths resource paths of test result reports
-     * @param expectedBuildResult expected build results for assertion
-     * @param setSkipMarkingBuildAsUnstableOnTestFailure configures freestyle job to skip marking build as unstable on test failure
-     * @param setAllowEmptyResults configures freestyle job to allow empty test result reports
-     * @param setRetainLogStandardOutputError configures freestyle job to retain log standard output error
-     * @return created and ran build
-     */
-    public static Build createFreeStyleJobAndRunBuild(AbstractJUnitTest abstractJUnitTestBaseClass,
-            List<String> resourcePaths, String expectedBuildResult,
-            Boolean setSkipMarkingBuildAsUnstableOnTestFailure, Boolean setAllowEmptyResults,
-            Boolean setRetainLogStandardOutputError) {
-        Build build = getCreatedFreeStyleJobWithResources(abstractJUnitTestBaseClass, resourcePaths,
-                setSkipMarkingBuildAsUnstableOnTestFailure, setAllowEmptyResults,
-                setRetainLogStandardOutputError).startBuild();
+        Build build = getCreatedFreeStyleJobWithResources(abstractJUnitTestBaseClass, resourcePaths).startBuild();
         assertThat(build.getResult()).isEqualTo(expectedBuildResult);
         build.open();
         return build;
@@ -55,6 +36,29 @@ public class TestUtils {
 
     /**
      * Creates a freestyle Job with resources.
+     *
+     * @param abstractJUnitTestBaseClass the caller's test class
+     * @param resourcePaths resource paths of test result reports
+     * @return created freestyle job.
+     */
+    public static Job getCreatedFreeStyleJobWithResources(AbstractJUnitTest abstractJUnitTestBaseClass,
+            List<String> resourcePaths) {
+        FreeStyleJob j = abstractJUnitTestBaseClass.jenkins.jobs.create();
+        FixedCopyJobDecorator fixedCopyJob = new FixedCopyJobDecorator(j);
+        fixedCopyJob.getJob().configure();
+        for (String resourcePath : resourcePaths) {
+            fixedCopyJob.copyResource(abstractJUnitTestBaseClass.resource(resourcePath));
+        }
+        JUnitPublisher publisher = fixedCopyJob.getJob().addPublisher(JUnitPublisher.class);
+        publisher.testResults.set("*.xml");
+
+        fixedCopyJob.getJob().save();
+        return fixedCopyJob.getJob();
+    }
+
+    /**
+     * Creates a freestyle Job with resources.
+     *
      * @param abstractJUnitTestBaseClass the caller's test class
      * @param resourcePaths resource paths of test result reports
      * @param setSkipMarkingBuildAsUnstableOnTestFailure configures freestyle job to skip marking build as unstable on test failure
@@ -63,51 +67,54 @@ public class TestUtils {
      * @return created freestyle job.
      */
     public static Job getCreatedFreeStyleJobWithResources(AbstractJUnitTest abstractJUnitTestBaseClass,
-            List<String> resourcePaths,
-            Boolean setSkipMarkingBuildAsUnstableOnTestFailure, Boolean setAllowEmptyResults,
-            Boolean setRetainLogStandardOutputError) {
+            List<String> resourcePaths, Boolean setSkipMarkingBuildAsUnstableOnTestFailure,
+            Boolean setRetainLogStandardOutputError, Boolean setAllowEmptyResults) {
         FreeStyleJob j = abstractJUnitTestBaseClass.jenkins.jobs.create();
-        FixedCopyJobDecorator fixedCopyJob = new FixedCopyJobDecorator(j);
-        fixedCopyJob.getJob().configure();
+        j.configure();
         for (String resourcePath : resourcePaths) {
-            fixedCopyJob.copyResource(abstractJUnitTestBaseClass.resource(resourcePath));
+            j.copyResource(abstractJUnitTestBaseClass.resource(resourcePath));
         }
-        JUnitJobConfiguration publisher = fixedCopyJob.getJob().addPublisher(JUnitJobConfiguration.class);
+
+        JUnitJobConfiguration publisher = j.addPublisher(JUnitJobConfiguration.class);
         publisher.testResults.set("*.xml");
 
         publisher.setSkipMarkingBuildAsUnstableOnTestFailure(setSkipMarkingBuildAsUnstableOnTestFailure);
         publisher.setRetainLogStandardOutputError(setRetainLogStandardOutputError);
         publisher.setAllowEmptyResults(setAllowEmptyResults);
 
-        fixedCopyJob.getJob().save();
-
-        return fixedCopyJob.getJob();
+        j.save();
+        return j;
     }
 
     /**
      * Creates a freestyle and runs two consecutive builds with different test result report which increases failure count
      * in the second build.
+     *
      * @param abstractJUnitTestBaseClass the caller's test class
      * @return second build
      */
     public static Build createTwoBuildsWithIncreasedTestFailures(AbstractJUnitTest abstractJUnitTestBaseClass) {
-        Job job = getCreatedFreeStyleJobWithResources(abstractJUnitTestBaseClass,
-                Arrays.asList("/failure/three_failed_two_succeeded.xml", "/failure/four_failed_one_succeeded.xml"),
-                false, false, false);
+        FreeStyleJob j = abstractJUnitTestBaseClass.jenkins.jobs.create();
+        FixedCopyJobDecorator fixedCopyJob = new FixedCopyJobDecorator(j);
+        fixedCopyJob.getJob().configure();
+        fixedCopyJob.copyResource(abstractJUnitTestBaseClass.resource("/failure/three_failed_two_succeeded.xml"));
+        fixedCopyJob.copyResource(abstractJUnitTestBaseClass.resource("/failure/four_failed_one_succeeded.xml"));
+        fixedCopyJob.getJob().addPublisher(JUnitPublisher.class).testResults.set("three_failed_two_succeeded.xml");
+        fixedCopyJob.getJob().save();
+        fixedCopyJob.getJob().startBuild().shouldBeUnstable();
 
-        job.startBuild().shouldBeUnstable();
-
-        job.configure();
-        job.editPublisher(JUnitJobConfiguration.class, (publisher) -> {
+        fixedCopyJob.getJob().configure();
+        fixedCopyJob.getJob().editPublisher(JUnitPublisher.class, (publisher) -> {
             publisher.testResults.set("four_failed_one_succeeded.xml");
         });
 
-        job.startBuild().shouldBeUnstable().openStatusPage();
-        return job.getLastBuild();
+        fixedCopyJob.getJob().startBuild().shouldBeUnstable().openStatusPage();
+        return fixedCopyJob.getJob().getLastBuild();
     }
 
     /**
      * Asserts given predicates within the given collection.
+     *
      * @param collection collection to be asserted
      * @param predicates assertion criteria
      * @param <ElementType> the type of elements in this collection
