b41bb858d4694ee00823f7da9966640b92ec8625
Michael Mueller <michael.mueller@hm.edu>
2022-01-20 23:39:36 +0100

    fixed tests

--- ui-tests/src/test/java/io/jenkins/plugins/analysis/junit/JobConfigurationTest.java
+++ ui-tests/src/test/java/io/jenkins/plugins/analysis/junit/JobConfigurationTest.java
@@ -5,8 +5,8 @@ import java.util.Collections;
 import org.junit.Test;
 
 import org.jenkinsci.test.acceptance.junit.AbstractJUnitTest;
-import org.jenkinsci.test.acceptance.po.Job;
-import io.jenkins.plugins.analysis.junit.util.TestUtils;
+import org.jenkinsci.test.acceptance.po.Build;
+import org.jenkinsci.test.acceptance.po.FreeStyleJob;
 
 import static org.assertj.core.api.AssertionsForClassTypes.*;
 
@@ -17,48 +17,54 @@ import static org.assertj.core.api.AssertionsForClassTypes.*;
  * @author Nikolas Paripovic
  */
 public class JobConfigurationTest extends AbstractJUnitTest {
-
     /**
      * Tests if build is successful with test failures when checkbox "Skip publishing checks" is checked.
      */
     @Test
-    public void verifySuccessfulBuildWhenSkipMarkingBuildAsUnstableOnTestFailureChecked() {
-        Job job = TestUtils.getCreatedFreeStyleJobWithResources(
-                this,
-                Arrays.asList("/failure/TEST-com.simple.project.AppTest.xml"),
-                true, false, false);
+    public void successfulBuildWhenSkipMarkingBuildAsUnstableOnTestFailureChecked() {
+        FreeStyleJob j = jenkins.jobs.create();
+        j.configure();
+        j.copyResource(resource("/failure/com.simple.project.AppTest.txt"));
+        j.copyResource(resource("/failure/TEST-com.simple.project.AppTest.xml"));
+        JUnitJobConfiguration publisher = j.addPublisher(JUnitJobConfiguration.class);
+        publisher.testResults.set("*.xml");
+        publisher.setSkipMarkingBuildAsUnstableOnTestFailure(true);
+        j.save();
 
-        job.startBuild().shouldSucceed();
+        Build build = j.startBuild();
+        assertThat(build.getResult()).isEqualTo("SUCCESS");
     }
 
     /**
      * Tests if build is successful with no test results when checkbox "Allow empty results" is checked.
      */
     @Test
-    public void verifySuccessfulBuildWhenEmptyTestResultsChecked() {
-        Job job = TestUtils.getCreatedFreeStyleJobWithResources(
-                this,
-                Collections.emptyList(),
-                false, false, true);
+    public void successfulBuildWhenEmptyTestResultsChecked() {
+        FreeStyleJob j = jenkins.jobs.create();
+        j.configure();
+        JUnitJobConfiguration publisher = j.addPublisher(JUnitJobConfiguration.class);
+        publisher.setAllowEmptyResults(true);
+        j.save();
 
-        job.startBuild().shouldSucceed();
+        j.startBuild().shouldSucceed();
     }
 
     /**
-     * Tests if long standard output is not truncated in test details when checkbox "Retain long standard output/error"
-     * is checked.
+     * Tests if long standard output is not truncated in test details when checkbox "Retain long standard output/error" is checked.
      */
     @Test
-    public void verifyRetainLongStandardOutputError() {
-        Job job = TestUtils.getCreatedFreeStyleJobWithResources(
-                this,
-                Arrays.asList("/success/junit-with-long-output.xml"),
-                false, true, false);
+    public void retainLongStandardOutputError() {
+        FreeStyleJob j = jenkins.jobs.create();
+        j.configure();
+        j.copyResource(resource("/success/junit-with-long-output.xml"));
+        JUnitJobConfiguration publisher = j.addPublisher(JUnitJobConfiguration.class);
+        publisher.testResults.set("*.xml");
+        publisher.setRetainLogStandardOutputError(true);
 
-        job.startBuild().shouldSucceed();
-
-        job.getJenkins().visit("/job/" + job.name + "/1/testReport/(root)/JUnit/testScore_0_/");
-        TestDetail testDetail = new TestDetail(job.getLastBuild());
+        j.save();
+        Build build = j.startBuild().shouldSucceed();
+        j.visit("/job/" + j.name + "/1/testReport/(root)/JUnit/testScore_0_/");
+        TestDetail testDetail = new TestDetail(build);
 
         assertThat(testDetail.getStandardOutput()).isPresent();
         assertThat(testDetail.getStandardOutput().get()).doesNotContain("truncated");
