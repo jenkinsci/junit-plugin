5c2848328b60fcf69b7df8ce60c13d3961be2daa
Jagruti Tiwari <tiwarijagruti1@gmail.com>
2022-10-03 13:45:35 +0530

    [JENKINS-69658] Move internal `script` block in "failed-test.jelly" to external file (#439)
    Co-authored-by: Tim Jacomb <21194782+timja@users.noreply.github.com>

---
+++ src/main/resources/lib/hudson/test/js/failureSummary.js
@@ -0,0 +1,20 @@
+
+function showFailureSummary(id,query) {
+    var element = document.getElementById(id)
+    element.style.display = "";
+    document.getElementById(id + "-showlink").style.display = "none";
+    document.getElementById(id + "-hidelink").style.display = "";
+
+    if (typeof query !== 'undefined') {
+        var rqo = new XMLHttpRequest();
+        rqo.open('GET', query, true);
+        rqo.onreadystatechange = function() { element.innerHTML = rqo.responseText; }
+        rqo.send(null);
+    }
+}
+
+function hideFailureSummary(id) {
+    document.getElementById(id).style.display = "none";
+    document.getElementById(id + "-showlink").style.display = "";
+    document.getElementById(id + "-hidelink").style.display = "none";
+}
